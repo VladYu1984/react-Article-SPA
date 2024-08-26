d74fcf3d79be093595bf17990cc9a6620067af2a
Vlad <bobilev.v@fmedia.tech>
2024-08-26 23:14:04 +0400

    #fix_add_ESLintFixes

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,19 +1,19 @@
 import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
-import { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/BuildCssLoader';
+import { BuildPaths } from '../build/types/config';
 
-export default ({ config } : { config: webpack.Configuration }) => {
+export default ({ config }: { config: webpack.Configuration }) => {
     const paths: BuildPaths = {
+        build: '',
         html: '',
         entry: '',
-        build: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
+    config.resolve?.modules?.push(paths.src);
+    config.resolve?.extensions?.push('.ts', '.tsx');
 
-    config?.resolve?.modules?.push(paths.src);
-    config?.resolve?.extensions?.push('.ts', '.tsx');
-
+    // eslint-disable-next-line no-param-reassign
     config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
@@ -22,11 +22,12 @@ export default ({ config } : { config: webpack.Configuration }) => {
         return rule;
     });
 
-    config?.module?.rules?.push({
+    config?.module?.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config?.module?.rules?.push(buildCssLoader(true));
+
+    config.module?.rules?.push(buildCssLoader(true));
 
     return config;
 };
