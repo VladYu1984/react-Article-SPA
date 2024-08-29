d0a71343b4d55f5f2904a953b814b21226e80b29
Vlad <bobilev.v@fmedia.tech>
2024-08-29 19:14:40 +0400

    #dev_addReduxRTK + #fsd_addEntities

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
-import { componentRouter } from 'shared/lib/tests/componentRender/componentRender';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('Test render', () => {
-        componentRouter(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('Test toggle', () => {
-        componentRouter(<Sidebar />);
+        componentRender(<Sidebar />);
         const btnToggle = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(btnToggle);
