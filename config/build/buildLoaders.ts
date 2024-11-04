22df5b59075a8e3d5ff5725185ddd1704a178f29
Vlad <bobilev.v@fmedia.tech>
2024-11-04 21:12:39 +0400

    #build Add newCustomTheme & reactRefreshPlugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,29 +1,16 @@
 import webpack from 'webpack';
+import { buildBabelLoader } from './loaders/BuildBabelLoader';
 import { buildCssLoader } from './loaders/BuildCssLoader';
 import { BuildOptions } from './types/config';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = {
-        test: /\.(?:js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        { locales: ['ru', 'en'], keyAsDefaultValue: true },
-                    ],
-                ],
-            },
-        },
-    };
+    const babelLoader = buildBabelLoader(options);
 
     const cssLoader = buildCssLoader(isDev);
 
